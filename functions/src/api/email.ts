97b09a6ed30dc5ff652ff6e03e64bc43326dad7a
Erick Maeda <erick.maeda26@gmail.com>
2020-03-21 16:12:51 +0000

    - Add email send;

--- functions/src/api/email.ts
+++ functions/src/api/email.ts
@@ -1,30 +1,57 @@
-import { errorResponse } from '../utils';
+import { errorResponse, successResponse } from '../utils';
+import { firestore } from 'firebase-admin';
 const express = require('express');
 const cors = require('cors');
 const sgMail = require('@sendgrid/mail');
-
 const api = express();
+
+sgMail.setApiKey("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");
 api.use(cors({ origin: true }));
 api.post(
     '/',
     async (request: any, response: any) => {
-        const msg = {
-            to: 'test@example.com',
-            from: 'test@example.com',
-            subject: 'Sending with Twilio SendGrid is Fun',
-            text: 'and easy to do anywhere, even with Node.js',
-            html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-        };
-        sgMail.send(msg);
+        try {
+            const { emailAddress, emailTemplateCode, name, reason, notes } = request.body;
+            if (!emailAddress) {
+                return response.status(500).send(errorResponse("The param emailAddress is required!"));
+            }
+            if (!emailTemplateCode) {
+                return response.status(500).send(errorResponse("The param emailTemplateCode is required!"));
+            }
+            if (!name) {
+                return response.status(500).send(errorResponse("The param name is required!"));
+            }
+            if (!reason) {
+                return response.status(500).send(errorResponse("The param reason is required!"));
+            }
+            if (!notes) {
+                return response.status(500).send(errorResponse("The param notes is required!"));
+            }
+    
+            console.info('Request body -> ', request.body);
+            const emailTemplate = await firestore().collection("emailTemplates").doc(emailTemplateCode).get()
+            const emailTemplateFirestore = emailTemplate.data();
+            const emailBodyHtml = emailTemplateFirestore?.template
+                .split("{NAME}")
+                .join(name)
+                .split("{REASON}")
+                .join(reason)
+                .split("{NOTES}")
+                .join(notes)
+                .split("{EMAIL}")
+                .join(emailAddress);
 
-        
-        console.log(request, request.body, request.params);
-        const { emailAddress, emailTemplateCode } = request.body;
-        if (!emailAddress) {
-            return response.status(500).send(errorResponse("The param emailAddress is required!"));
-        }
-        if (!emailTemplateCode) {
-            return response.status(500).send(errorResponse("The param emailTemplateCode is required!"));
+            console.info('emailTemplateFirestore', emailTemplateFirestore);
+
+            sgMail.send({
+                to: emailTemplateFirestore?.to,
+                from: emailTemplateFirestore?.from,
+                subject: emailTemplateFirestore?.subject,
+                html: emailBodyHtml,
+            });
+            return response.send(successResponse());
+        } catch (error) {
+            return response.send(errorResponse(error.message));
         }
     }
 );
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import emailApi from './api/email';
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 admin.initializeApp();
 
